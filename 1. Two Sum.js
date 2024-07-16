d837c52f849b9f2a898b906d7cb7940680ef38f3
OfirIsHere <Ofirdoron19@gmail.com>
2024-07-16 17:13:59 +0300

    task 1 solution

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -4,8 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if ((target === nums[i] + nums[j]) && i !== j) {
+                return [i, j]
+            }
+        }
+    }
+}
 
+// console.log(twoSum([3, 2, 4], 6));
+
+var twoSumOn1 = function (nums, target) {
+    let map = new Map()
+
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i]
+        if(map.has(complement)) {
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i)
+    }
+
+    return undefined
 }
 
 
-twoSum([2, 7, 11, 15], 9)
+console.log(twoSumOn1([3, 2, 4], 6));
